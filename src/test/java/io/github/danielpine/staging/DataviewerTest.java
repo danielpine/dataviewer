4649a59d99bd1f9f5e283603c4aefc5915a79b9d
danielpine <danielpine@sina.com>
2021-02-23 11:52:50 +0800

    Add test resource folder.

---
+++ src/test/java/io/github/danielpine/staging/DataviewerTest.java
@@ -0,0 +1,13 @@
+package io.github.danielpine.staging;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+
+@RunWith(BlockJUnit4ClassRunner.class)
+public class DataviewerTest {
+	@Test
+	public void testDisplayTable() {
+		System.out.println("hi");
+	}
+}
